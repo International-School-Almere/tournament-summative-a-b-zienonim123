0e383ed3fa2b31c09dd554e87971ab4ea3863774
zienonim123 <136816@secondary.isalmere.nl>
2026-04-11 14:33:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-#main file for the Tournament App.
+# main file for the Tournament App.
+
 teams = []
 individuals = []
 events = []
 scores = {}
+
+
+def menu():
+    running = True
+    while running:
+        choice = input("Choose option: ")
+
+        if choice == "1":
+            print("add individual to the teams")
+        elif choice == "2":
+            print("second option")
+        
+        elif choice == "3":
+            print("second option")
+        else:
+            print("wrong option")
