d9a7f07a028890a4b259301687f57b00436f899f
zienonim123 <136816@secondary.isalmere.nl>
2026-04-14 14:01:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,24 @@ scores = {}
 def menu():
     running = True
     while running:
+        print("1 - add individual to the teams")
+        print("2 - add team")
+        print("3 - add event")
+        print("4 - view scores")
         choice = input("Choose option: ")
 
         if choice == "1":
             print("add individual to the teams")
+
         elif choice == "2":
-            print("second option")
-        
+            print("add team")
+
         elif choice == "3":
-            print("second option")
+            print("add event")
+        
+        elif choice == "4":
+            print("view scores")
         else:
             print("wrong option")
+
+menu()  
